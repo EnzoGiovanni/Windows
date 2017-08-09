6b98dc7714d9926a7aefcd2da8783ed5b369bf73
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2017-08-09 13:24:20 +0200

    Update Replace String.ps1

--- PowerShell/Replace String.ps1
+++ PowerShell/Replace String.ps1
@@ -5,6 +5,5 @@ remplacer une donnée à une position fixe
 
 $NewString = ""#chaine de remplacement
 $old = "" #chaine dans laquelle effectuer le remplacement
-$ColDeb = 1 #position de début
-$ColFin = 1 #position de fin
-$new = $old.remove($ColDeb,$ColFin).insert($ColDeb,$NewString)
+$ColDeb = 1 #position de début de la chaine
+$new = $old.remove($ColDeb, $NewString.Length).insert($ColDeb, $NewString)
