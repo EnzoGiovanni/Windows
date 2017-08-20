1df412ca1ed80f9289b527eee1223fc069e6ff00
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2017-08-20 23:09:49 +0200

    Update Lister Extentions.ps1

--- PowerShell/Lister Extentions.ps1
+++ PowerShell/Lister Extentions.ps1
@@ -1,5 +1,5 @@
 $REP = ""
 
-$Exts = Get-ChildItem $REP -Recurse -Force -exclude *.jpg, *.JPG, *.jpeg, *.Rw2 |Select-Object -Property Extension
+$Exts = Get-ChildItem $REP -Recurse -Force -File -exclude *.jpg, *.JPG, *.jpeg, *.Rw2 |Select-Object -Property Extension
 
 $Exts  | Sort-Object Extension | Set-Content "C:\Users\Olivier\Downloads\Extentions.txt"
