df9227a591a2656bd457f8e4fe056fff602f6066
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2017-10-25 11:49:49 +0200

    Create Normalise CSV.ps1

---
+++ PowerShell/Normalise CSV.ps1
@@ -0,0 +1,16 @@
+#Normalisation d'un CSV
+
+
+$Rep = (Get-Item env:\USERPROFILE).value + "\Downloads\"
+$FileIn = $Rep + "FichierIn.csv"
+$FileOut = $Rep + "fichierOut.csv"
+
+$Lignes = Get-Content $FileIn
+
+ForEach ($Ligne In $Lignes)
+{
+    $Elts = $Ligne.split(";")
+    $NewLine = ""
+    ForEach ($Elt In $Elts){$NewLine = $NewLine + '"' + $Elt + '"' + ';'}
+    $NewLine | Add-Content $FileOut
+}
