bbdf91ddd38c4cc5f23ff71583616cab53b4277a
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2025-12-16 17:31:44 +0100

    Update Ouvrir une session Edge isolee pour un site web.ps1

--- PowerShell/Ouvrir une session Edge isolee pour un site web.ps1
+++ PowerShell/Ouvrir une session Edge isolee pour un site web.ps1
@@ -1,4 +1,10 @@
+# ouvrir une session du navigateur EDGE isolée,
+# à chaque éxécution créer un repertoire de travail temporaire unique
+# puis lance edge avec ce repertoire de travail dans une nouvelle fenêtre privée du navigateur,
+# ce qui a pour effet de créer une nouvelle instance d'exécution de edge (nouveau PID)
 
+# ce sript est à appeler depuis un racourcis windows tel que :
+# C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%USERPROFILE%\{chemin}\nom du script script.ps1"
 # Crée un dossier temporaire unique pour le profil
 $d = Join-Path $env:TEMP ("EdgeIso-" + (Get-Random))
 
@@ -6,7 +12,7 @@ $d = Join-Path $env:TEMP ("EdgeIso-" + (Get-Random))
 $edge = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 
 # Arguments : profil jetable + InPrivate + nouvelle fenêtre
-$args = @("--user-data-dir=$d","--inprivate","--new-window https://reftech-adm-rec1.internal.fd-recette.net/login")
+$args = @("--user-data-dir=$d","--inprivate","--new-window https://domaine/index.com")
 
 # Lancer Edge
 Start-Process $edge -ArgumentList $args
