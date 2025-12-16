5ab8c00610f613c8ca1aa3bcd745c2e769bc3d36
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2025-12-16 17:29:17 +0100

    Update Ouvrir une session Edge isolee.ps1

--- PowerShell/Ouvrir une session Edge isolee.ps1
+++ PowerShell/Ouvrir une session Edge isolee.ps1
@@ -1,3 +1,11 @@
+# ouvrir une session du navigateur isolée,
+# à chaque éxécution créer un repertoire de travail temporaire unique
+# puis lance edge avec ce repertoire de travail dans une nouvelle fenêtre privée du navigateur,
+# ce qui a pour effet de créer une nouvelle instance d'exécution de edge (nouveau PID)
+
+# ce sript est à appeler depuis un racourcis windows tel que :
+# C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%USERPROFILE%\{chemin}\nom du script script.ps1"
+
 
 # Crée un dossier temporaire unique pour le profil
 $d = Join-Path $env:TEMP ("EdgeIso-" + (Get-Random))
